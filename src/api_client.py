8ef3ac444b180497168da07cf83b6e1338f67850
hannah cushman garland <hannah.cushman@gmail.com>
2023-06-21 14:43:39 -0500

    Fix tests

--- src/api_client.py
+++ src/api_client.py
@@ -8,14 +8,20 @@ class AuthenticationError(Exception):
 
 
 class EDSAPIClient(scrapelib.RetrySession):
-    def __init__(self, client_id, client_secret, domain='https://apiqa.chicago.gov'):
+    def __init__(
+        self,
+        client_id,
+        client_secret,
+        domain="https://apiqa.chicago.gov",
+        retry_attempts=2,
+    ):
         super().__init__()
-        self._retry_attempts = 2
+        self._retry_attempts = retry_attempts
         self._retry_wait_seconds = 0.1
         self.domain = domain
 
         if not all([client_id, client_secret]):
-            raise ValueError('Must provide a client_id and a client_secret.')
+            raise ValueError("Must provide a client_id and a client_secret.")
         else:
             self.client_id = client_id
             self.client_secret = client_secret
@@ -26,41 +32,46 @@ class EDSAPIClient(scrapelib.RetrySession):
         url = "{domain}/edsapi/oauth/token".format(domain=self.domain)
 
         body = {
-            'username': username,
-            'password': password,
-            'grant_type': 'password',
-            'scope': 'write',
-            'client_id': self.client_id,
-            'client_secret': self.client_secret
+            "username": username,
+            "password": password,
+            "grant_type": "password",
+            "scope": "write",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
         }
 
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
         response = self.post(url, data=body, headers=headers)
 
         if response.status_code == 200:
             return response
         else:
-            logging.error('Failed to get access token for username: {username}'.format(username=username))
-            raise AuthenticationError({'status_code': response.status_code,
-                                       'content': str(response.content)})
+            logging.error(
+                "Failed to get access token for username: {username}".format(
+                    username=username
+                )
+            )
+            raise AuthenticationError(
+                {"status_code": response.status_code, "content": str(response.content)}
+            )
 
     def submit_eds(self, eds_data, username, password):
         """Submits an EDS. Requires the EDS data and the vendor's username/password."""
 
         if not all([eds_data, username, password]):
-            raise ValueError('Must provide eds_data, a username, and a password.')
+            raise ValueError("Must provide eds_data, a username, and a password.")
 
         try:
-            access_token_response = self._get_access_token(username=username, password=password)
+            access_token_response = self._get_access_token(
+                username=username, password=password
+            )
 
-            access_token = access_token_response.json()['access_token']
+            access_token = access_token_response.json()["access_token"]
 
             headers = {
-                'Authorization': "Bearer {token}".format(token=access_token),
-                'Content-Type': 'application/json'
+                "Authorization": "Bearer {token}".format(token=access_token),
+                "Content-Type": "application/json",
             }
 
             url = "{domain}/edsapi/rest/eds/".format(domain=self.domain)
@@ -74,16 +85,20 @@ class EDSAPIClient(scrapelib.RetrySession):
         """Registers a new user. Requires the application's username and password."""
 
         if not all([new_user_data, app_username, app_password]):
-            return ValueError('Must provide new_user_data, an app_username, and an app_password.')
+            return ValueError(
+                "Must provide new_user_data, an app_username, and an app_password."
+            )
 
         try:
-            access_token_response = self._get_access_token(username=app_username, password=app_password)
+            access_token_response = self._get_access_token(
+                username=app_username, password=app_password
+            )
 
-            access_token = access_token_response.json()['access_token']
+            access_token = access_token_response.json()["access_token"]
 
             headers = {
-                'Authorization': "Bearer {token}".format(token=access_token),
-                'Content-Type': 'application/json'
+                "Authorization": "Bearer {token}".format(token=access_token),
+                "Content-Type": "application/json",
             }
 
             url = "{domain}/edsapi/rest/user/".format(domain=self.domain)
@@ -98,10 +113,12 @@ class EDSAPIClient(scrapelib.RetrySession):
         A 500 might happen with invalid login credentials.
         """
         try:
-            error_message = response.json().get('status', None)
+            error_message = response.json().get("status", None)
         except json.decoder.JSONDecodeError:
             # occurs when the API returns an XML response
             error_message = None
-        return (super().accept_response(response)
-                or response.status_code == 500
-                or 'Validation errors in your request' in error_message)
+        return (
+            super().accept_response(response)
+            or response.status_code == 500
+            or "Validation errors in your request" in error_message
+        )
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,66 +10,68 @@ from src.web_client import EDSWebClient
 @pytest.fixture
 def app_creds():
     return {
-        'client_id': os.environ['EDS_CLIENT_ID'],
-        'client_secret': os.environ['EDS_CLIENT_SECRET'],
-        'eds_domain': os.environ['EDS_DOMAIN_NAME'],
-        'username': os.environ['EDS_CLIENT_USERNAME'],
-        'password': os.environ['EDS_CLIENT_PASSWORD']
+        "client_id": os.environ["EDS_CLIENT_ID"],
+        "client_secret": os.environ["EDS_CLIENT_SECRET"],
+        "eds_domain": os.environ["EDS_DOMAIN_NAME"],
+        "username": os.environ["EDS_CLIENT_USERNAME"],
+        "password": os.environ["EDS_CLIENT_PASSWORD"],
     }
 
 
 @pytest.fixture
 def api_client(app_creds):
-    return EDSAPIClient(client_id=app_creds['client_id'],
-                        client_secret=app_creds['client_secret'],
-                        domain=app_creds['eds_domain'])
+    return EDSAPIClient(
+        client_id=app_creds["client_id"],
+        client_secret=app_creds["client_secret"],
+        domain=app_creds["eds_domain"],
+        retry_attempts=0,
+    )
 
 
 @pytest.fixture
 def web_client():
-    return EDSWebClient(os.environ['TEST_VENDOR_USERNAME'], os.environ['TEST_VENDOR_PASSWORD'])
+    return EDSWebClient(
+        os.environ["TEST_VENDOR_USERNAME"],
+        os.environ["TEST_VENDOR_PASSWORD"],
+        retry_attempts=0,
+    )
 
 
 @pytest.fixture
 def user():
-    username = os.environ['TEST_VENDOR_USERNAME']
-    password = os.environ['TEST_VENDOR_PASSWORD']
+    username = os.environ["TEST_VENDOR_USERNAME"]
+    password = os.environ["TEST_VENDOR_PASSWORD"]
     return username, password
 
 
 @pytest.fixture
 def valid_submission():
-    with open('tests/fixtures/submission_valid.json') as f:
+    with open("tests/fixtures/submission_valid.json") as f:
         valid_submission = json.load(f)
     return valid_submission
 
 
 @pytest.fixture
 def invalid_submission():
-    with open('tests/fixtures/submission_invalid.json') as f:
+    with open("tests/fixtures/submission_invalid.json") as f:
         invalid_submission = json.load(f)
     return invalid_submission
 
 
 @pytest.fixture
 def new_user():
-    with open('tests/fixtures/new_user_valid.json') as f:
+    with open("tests/fixtures/new_user_valid.json") as f:
         new_user = json.load(f)
 
     applicant = unique_applicant()
-    new_user['user'].update({
-        'userId': applicant[0],
-        'email': applicant[1]
-    })
+    new_user["user"].update({"userId": applicant[0], "email": applicant[1]})
 
-    new_user['company'].update({
-        'ssn': applicant[2]
-    })
+    new_user["company"].update({"ssn": applicant[2]})
     return new_user
 
 
 @pytest.fixture
 def invalid_user():
-    with open('tests/fixtures/new_user_invalid.json') as f:
+    with open("tests/fixtures/new_user_invalid.json") as f:
         invalid_user = json.load(f)
     return invalid_user
